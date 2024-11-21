e83d094c77d9fb4f0d683e382d68fc3d6f1bd5bd
Guylène D <165901249+guylened@users.noreply.github.com>
2024-11-21 18:48:45 +0100

    banner

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,31 +1,17 @@
-import { useState } from 'react'
-import kasaLogo from '../assets/logo.png'
+import paysage1 from '../assets/paysage1.png'
 import '../styles/App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
+    return (
     <>
-      <div>
-        <a  target="_blank">
-          <img src={kasaLogo} className="logo" alt="Kasa logo" />
-        </a>        
+      <div className="banner">
+      <img src={paysage1}  alt="bannière paysage" />  
+       <h1>Chez vous, partout et ailleurs</h1>               
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
+
 
 export default App
